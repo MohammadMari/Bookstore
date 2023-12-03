b6417a314cbf90a3b573b14dbc58e4f6717df583
Fatima Shibli <blackspaider99@gmail.com>
2023-12-03 00:02:23 -0600

    photos added

--- src/components/BookTile.js
+++ src/components/BookTile.js
@@ -5,8 +5,9 @@ import './BookTile.css';
 function BookTile(props) {
     const { bookTileInfo, onAddToCart } = props;
 
-    const sanitizedTitle = bookTileInfo.title.replace(/\s/g, '_').toLowerCase();
-    const imageUrl = `${process.env.PUBLIC_URL}/images/${sanitizedTitle}.jpg`;
+    // Ensure sanitizedTitle is not undefined
+    const sanitizedTitle = bookTileInfo.title ? bookTileInfo.title.replace(/\s/g, '_').toLowerCase() : 'placeholder';
+    const imageUrl = `/images/${sanitizedTitle}.jpg`;
 
     console.log("Generated Image URL:", imageUrl);
 
@@ -18,7 +19,12 @@ function BookTile(props) {
     return (
         <div className="book-tile">
             <div className='book-picture'>
-                <img src={bookTileInfo.image} alt={`${bookTileInfo.title}`} />
+                <img
+                    src={imageUrl}
+                    alt={`${bookTileInfo.title}`}
+                    style={{ width: "100%", height: "100%", objectFit: 'contain' }}
+                    onError={(e) => { e.target.src = '/images/placeholder.jpg'; }}
+                />
             </div>
             <div className='book-info'>
                 <div className='title'>{bookTileInfo.title}</div>
